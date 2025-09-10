ff0756c62d0ed85688e4e00e44cf5ebd0bdf8cf5
Laxmi Narayan <146916452+laxminarayan-dev@users.noreply.github.com>
2025-09-10 11:30:28 +0530

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -23,4 +23,5 @@ def predict():
     return jsonify({'response': response.text})
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=True
